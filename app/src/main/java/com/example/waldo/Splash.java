e226c4f0b6031021a6ed4657cdd7f77f6e3b9ebb
Manpreet7377 <manpreet7377@gmail.com>
2019-08-20 19:08:48 -0700

    Run time Permission

--- app/src/main/java/com/example/waldo/Splash.java
+++ app/src/main/java/com/example/waldo/Splash.java
@@ -1,7 +1,12 @@
 package com.example.waldo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,19 +14,78 @@ import android.util.Log;
 import com.example.waldo.activities.LoginActivity;
 import com.example.waldo.activities.MainDashborad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Splash extends AppCompatActivity {
+
+    String[] permissions = new String[]{
+            Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            call_permissions();
+
+        } else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
                     startActivity(new Intent(Splash.this, LoginActivity.class));
                     finish();
 
+                }
+            }, 1000);
+        }
+
+
+    }
+
+    private void call_permissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(getApplicationContext(), p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
             }
-        }, 1000);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray
+                    (new String[listPermissionsNeeded.size()]), 100);
+        }
+        return;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            startActivity(new Intent(Splash.this, LoginActivity.class));
+                            finish();
+
+                        }
+                    }, 1000);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            startActivity(new Intent(Splash.this, LoginActivity.class));
+                            finish();
+
+                        }
+                    }, 1000);                }
+                return;
+            }
+        }
     }
 }
